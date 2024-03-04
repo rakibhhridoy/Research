30a3dccf3941accf185b2aa358cb165605777225
rakibhhridoy <rakibhhridoy@yahoo.com>
2024-03-05 04:12:50 +0600

    Param A ++

--- Colab/Turag/Python/DL Lab/Univariate/pHA/feed_data.py
+++ Colab/Turag/Python/DL Lab/Univariate/pHA/feed_data.py
@@ -5,13 +5,13 @@ from sklearn.model_selection import train_test_split
 
 WINDOW_SIZE = 3
 
-fname = "/content/drive/MyDrive/Turag/Python/DL Lab/Univariate/data/LocationA.csv"
+fname = "/content/drive/MyDrive/Turag/Python/DL Lab/Univariate/phA/data/LocationA.csv"
 def data_extract():
     locA = load_file(fname)
     locA = locA.set_index("Date")
 
-    doA = locA["DOA"]
-    X1, y1 = df_to_X_y(doA, WINDOW_SIZE)
+    pHA = locA["pHA"]
+    X1, y1 = df_to_X_y(pHA, WINDOW_SIZE)
     x_train, x_test, y_train, y_test = train_test_split(X1, y1, test_size=0.2, random_state=1)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=1)    
     return x_train, x_test, y_train, y_test, x_val, y_val
